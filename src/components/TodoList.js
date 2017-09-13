658d85e04588b4b8679f1bf41127fbd0448af67a
incuisting <xuyanting9634@163.com>
2017-09-13 15:17:45 +0800

    update

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,13 +2,17 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Todo from './Todo'
 
-const TodoList = ({ todos, onTodoClick }) => (
+const TodoList = ({ todos, onTodoClick }) => {   
+    return (
     <ul>
-        {todos.map(todo => (
-            <Todo key={todo.id} {...todo} onClick={() => onTodoClick(todo.id)} />
-        ))}
+        {todos.map(todo =>{ 
+            return (
+            <Todo key={todo.id} {...todo}  onClick={() => onTodoClick(todo.id)} />
+        )}
+        )}
     </ul>
 )
+}    
 
 TodoList.propTypes = {
     todos: PropTypes.arrayOf(
